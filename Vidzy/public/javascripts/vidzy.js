e86d6c2fc7775f1c80f97bc03be299023be496e0
jiawku <jiawku@hotmail.com>
2017-11-01 09:24:14 -0500

    update

--- Vidzy/public/javascripts/vidzy.js
+++ Vidzy/public/javascripts/vidzy.js
@@ -1,11 +1,23 @@
-var app = angular.module('Vidzy', ['ngRoute']);
+var app = angular.module('Vidzy', ['ngResource','ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/', {
-            templateUrl: 'partials/home.html'
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        })
+        .when('/add-video', {
+            templateUrl: 'partials/video-form.html'
         })
         .otherwise({
             redirectTo: '/'
         });
 }]);
+
+app.controller('HomeCtrl', ['$scope', '$resource',
+    function($scope, $resource){
+      var Videos = $resource('/api/videos');
+      Videos.query(function(videos){
+          $scope.videos = videos;
+      });
+    }]);
